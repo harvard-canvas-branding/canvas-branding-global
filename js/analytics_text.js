d1ebe3e4dd10e2a9cf14c2da83f798927fc1823c
Parker, Eric P <eric_parker@harvard.edu>
2015-08-13 15:22:03 -0400

    added $(document).ready(function(e) {

--- js/analytics_text.js
+++ js/analytics_text.js
@@ -1,13 +1,16 @@
-var p = window.location.pathname.split("/");
-var filename = p[p.length-1];
 
-if(filename == "analytics") {
+$(document).ready(function() {
+	var p = window.location.pathname.split("/");
+	var filename = p[p.length-1];
 
-  var guidance_text = '<p id="guidance_text">The Faculty Oversight Committee on the Access to Electronic Information provides'+ 
-    ' <a href="https://wiki.harvard.edu/confluence/display/canvas/Electronic+Information+in+Canvas" target="_blank">guidance to faculty</a>'+
-    ' on using these analytics.</p>';
+	if(filename == "analytics") {
+
+	  var guidance_text = '<p id="guidance_text">The Faculty Oversight Committee on the Access to Electronic Information provides'+ 
+	    ' <a href="https://wiki.harvard.edu/confluence/display/canvas/Electronic+Information+in+Canvas" target="_blank">guidance to faculty</a>'+
+	    ' on using these analytics.</p>';
 
 
-  $('.course_graphs').before(guidance_text);
+	  $('.course_graphs').before(guidance_text);
 
-}
+	}
+});
