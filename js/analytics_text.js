382d53c726aacfca6eac2cd73e54a73b2d8b8d3d
Parker, Eric P <eric_parker@harvard.edu>
2015-08-17 14:19:54 -0400

    updated line format

--- js/analytics_text.js
+++ js/analytics_text.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
 
         var guidance_text = '<div class="center-alert-info">' +
                             '	<div class="alert-info">' +
-                            '		The Faculty Oversight Committee on the Access to Electronic Information provides'+
+                            '		The Faculty Oversight Committee on the Access to Electronic Information provides' +
                             ' 		<a href="https://wiki.harvard.edu/confluence/display/canvas/Electronic+Information+in+Canvas" target="_blank">' +
-                            ' 		<strong>guidance to faculty</strong></a>'+' on using these analytics.' +
+                            ' 		<strong>guidance to faculty</strong></a> on using these analytics.' +
                             '	</div>' +
                             '</div>';
 
