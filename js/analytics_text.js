5de9b4b3dcbd824932495c82f10a8cf354a72ec2
Parker, Eric P <eric_parker@harvard.edu>
2015-08-17 15:27:52 -0400

    testing

--- js/analytics_text.js
+++ js/analytics_text.js
@@ -1,23 +1,25 @@
 
-// get the name of the current page
-var p = window.location.pathname.split("/");
-var filename = p[p.length - 1];
+$(function(){
+	// get the name of the current page
+	var p = window.location.pathname.split("/");
+	var filename = p[p.length - 1];
 
-// if we are on the analytics page display the analytics text block
-if (filename == "analytics") {
+	// if we are on the analytics page display the analytics text block
+	if (filename == "analytics") {
 
-	var guidance_text = '<div class="center-alert-info">' +
-		'	<div class="alert-info">' +
-		'		The Faculty Oversight Committee on the Access to Electronic Information provides' +
-		' 		<a href="https://wiki.harvard.edu/confluence/display/canvas/Electronic+Information+in+Canvas" target="_blank">' +
-		' 		<strong>guidance to faculty</strong></a> on using these analytics.' +
-		'	</div>' +
-		'</div>';
+		var guidance_text = '<div class="center-alert-info">' +
+			'	<div class="alert-info">' +
+			'		The Faculty Oversight Committee on the Access to Electronic Information provides' +
+			' 		<a href="https://wiki.harvard.edu/confluence/display/canvas/Electronic+Information+in+Canvas" target="_blank">' +
+			' 		<strong>guidance to faculty</strong></a> on using these analytics.' +
+			'	</div>' +
+			'</div>';
 
-	// place the text block above the graphs on the page
-	$('.course_graphs').before(guidance_text);
+		// place the text block above the graphs on the page
+		$('.course_graphs').before(guidance_text);
 
-}
+	}
+});
 
 
 
