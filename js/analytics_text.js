db7c687e734a8eb95274b7c33c91de42562bc9c4
Parker, Eric P <eric_parker@harvard.edu>
2015-08-17 15:13:37 -0400

    testing

--- js/analytics_text.js
+++ js/analytics_text.js
@@ -1,19 +1,21 @@
 
-    var p = window.location.pathname.split("/");
-    var filename = p[p.length-1];
+$(document).ready(function() {
+	var p = window.location.pathname.split("/");
+	var filename = p[p.length - 1];
 
-    if(filename == "analytics") {
+	if (filename == "analytics") {
 
-        var guidance_text = '<div class="center-alert-info">' +
-                            '	<div class="alert-info">' +
-                            '		The Faculty Oversight Committee on the Access to Electronic Information provides' +
-                            ' 		<a href="https://wiki.harvard.edu/confluence/display/canvas/Electronic+Information+in+Canvas" target="_blank">' +
-                            ' 		<strong>guidance to faculty</strong></a> on using these analytics.' +
-                            '	</div>' +
-                            '</div>';
+		var guidance_text = '<div class="center-alert-info">' +
+			'	<div class="alert-info">' +
+			'		The Faculty Oversight Committee on the Access to Electronic Information provides' +
+			' 		<a href="https://wiki.harvard.edu/confluence/display/canvas/Electronic+Information+in+Canvas" target="_blank">' +
+			' 		<strong>guidance to faculty</strong></a> on using these analytics.' +
+			'	</div>' +
+			'</div>';
 
-        $('.course_graphs').before(guidance_text);
+		$('.course_graphs').before(guidance_text);
 
-    }
+	}
+});
 
 
