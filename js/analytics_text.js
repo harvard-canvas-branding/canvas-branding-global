9fe7b328399f08ae0ed45491ab0ed7f5f59cd44a
Parker, Eric P <eric_parker@harvard.edu>
2015-08-18 10:58:37 -0400

    Changed tab size to 2 per PR comment Updated/added comments This will only add the text block to the course_graphs container when it appears on the page if it has not already been added. There is an account level analytics page that we are not going to deal with in this story. if that is needed it will be added as a future story.

--- js/analytics_text.js
+++ js/analytics_text.js
@@ -2,11 +2,10 @@
 function addAnalyticsTextBlock() {
 	// get the name of the current page
 	var p = window.location.pathname.split("/");
-	var filename = p[p.length - 1];
+	var resource = p[p.length - 1];
 
 	// if we are on the analytics page display the analytics text block
-	if (filename == "analytics") {
-
+	if (resource == "analytics") {
 		var guidance_text = '<div class="center-alert-info">' +
 			'	<div class="alert-info">' +
 			'		The Faculty Oversight Committee on the Access to Electronic Information provides' +
@@ -24,32 +23,29 @@ function addAnalyticsTextBlock() {
 
 function initAnalyticsTextBlock(){
 
-    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-    var analyticsObserver = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-
+	MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+	var analyticsObserver = new MutationObserver(function (mutations) {
+		mutations.forEach(function (mutation) {
+			// check to see if the text block has already been added
 			if( $('.center-alert-info').length == 0) {
-
 				var $target = $(mutation.target);
-
+				// if the course_graphs class exists, the analytics have been loaded
+				// so go ahead and add the text block
 				if ($target.find('.course_graphs').length != 0) {
-
 					addAnalyticsTextBlock();
-					//console.log(mutation);
 				}
-
 			}
-        });
-    });
+		});
+	});
 
-    var $config = {
-        childList: true,
-        subtree: true,
-        attributes: false,
-        characterData: false,
-    };
+	var $config = {
+			childList: true,
+			subtree: true,
+			attributes: false,
+			characterData: false,
+	};
 
-    analyticsObserver.observe(document.body, $config);
+	analyticsObserver.observe(document.body, $config);
 
 }
 
