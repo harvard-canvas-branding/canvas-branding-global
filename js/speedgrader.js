c677a5b68acd892f60b36d621c91d39b1899aabc
Douglas Hall <douglas_hall@harvard.edu>
2015-09-14 19:36:55 +0000

    Update href string comparison

--- js/speedgrader.js
+++ js/speedgrader.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    if ($.browser.mozilla && window.location.href.indexOf('gradebook/speed_grader')) {
+    if ($.browser.mozilla && window.location.href.indexOf('gradebook/speed_grader') > -1) {
         $('#left_side_inner').prepend(
             '<div id="autosave_warning" class="ic-flash-error">' +
             'Warning for Firefox users: assignment comments will not be saved automatically -- you must click outside the comment box to save your last entry before navigating to another page. Please use another browser such as Chrome to take advantage of automatic saving.' +
