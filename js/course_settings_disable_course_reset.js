5a7a6afc9133d46187f5e22b97483e780f7bc050
Parker, Eric P <eric_parker@harvard.edu>
2015-11-19 16:11:39 -0500

    added js file to disable the course reset button on the settings page

---
+++ js/course_settings_disable_course_reset.js
@@ -0,0 +1,24 @@
+function isResetCourseButtonPresent(){
+    $resetBtn =$('.reset_course_content_button');
+    if ($resetBtn.length > 0 )
+        return true;
+    return false;
+}
+
+function disableCourseResetButton(){
+  $('.reset_course_content_button').removeAttr("href").addClass('disabled');
+}
+
+function initHUGlobal() {
+  var reCourseSettingsPage = /courses\/.+?\/settings/;
+  var windowUrl = window.location.pathname;
+  var onCourseSettingsPage = (windowUrl.search(reCourseSettingsPage) != -1);
+
+  if (onCourseSettingsPage) {
+    if (isResetCourseButtonPresent()) {
+        disableCourseResetButton();
+    }
+  }
+}
+
+$(document).ready(initHUGlobal);
