83eb49c2599c816d9198687016ef4595965eed3f
Parker, Eric P <eric_parker@harvard.edu>
2015-11-20 13:20:45 -0500

    checking if the right nav has mutations, I think it does not.

--- js/course_settings_disable_course_reset.js
+++ js/course_settings_disable_course_reset.js
@@ -14,9 +14,25 @@ function initDisableCourseReset() {
   var windowUrl = window.location.pathname;
   var onCourseSettingsPage = (windowUrl.search(reCourseSettingsPage) != -1);
 
-  if (onCourseSettingsPage) {
-    if (isResetCourseButtonPresent()) {
-        disableCourseResetButton();
+  if (onCourseSettingsPage && isResetCourseButtonPresent()) {
+    disableCourseResetButton();
+
+    var classAttrObserver = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        console.log(mutation.type);
+        console.log(mutation.target);
+      })
+    });
+
+    // content is the closest parent that's reliably availble on document.ready()
+    var $content = $('body');
+    if ($content.length > 0) {
+      classAttrObserver.observe($content.get(0), {
+        childList: true,
+        subtree: true,
+        attributes: true,
+        characterData: false
+      });
     }
   }
 }
