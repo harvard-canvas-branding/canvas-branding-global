349ce1b0ccd5ab16988f6928199355a682268d89
Parker, Eric P <eric_parker@harvard.edu>
2015-11-24 10:29:12 -0500

    added comments

--- js/account_settings_disable_add_user_and_course.js
+++ js/account_settings_disable_add_user_and_course.js
@@ -1,22 +1,29 @@
 function isAddUserButtonPresent(){
-    $btn =$('a.add_user_link');
+    $btn = $('a.add_user_link');
     return $btn.length > 0;
 }
 
 function isAddCourseButtonPresent(){
-    $btn =$('a.add_course_link');
+    $btn = $('a.add_course_link');
     return $btn.length > 0;
 }
 
 function disableAddUserButton(){
+  // remove the href asd well so people can't get the url
+  // and try it directly
   $('a.add_user_link').removeAttr("href").addClass('disabled');
 }
 
 function disableAddCourseButton(){
+  // remove the href asd well so people can't get the url
+  // and try it directly
   $('a.add_course_link').removeAttr('href').addClass('disabled');
 }
 
 function initDisableAddUserCourse() {
+  // These button appear on multiple pages in the account area.
+  // So far I have found them on the root account page, the settings page,
+  // and the users page. There could be other pages as well.
   var reAccountPage = /accounts\/.+?/;
   var windowUrl = window.location.pathname;
   var onAccountPage = (windowUrl.search(reAccountPage) != -1);
@@ -25,6 +32,7 @@ function initDisableAddUserCourse() {
     if (isAddUserButtonPresent()) {
       disableAddUserButton();
     }
+
     if(isAddCourseButtonPresent()){
       disableAddCourseButton();
     }
