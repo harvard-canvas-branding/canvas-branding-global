beeb03b0a819e2a9d969468bf52b3ce831a4e9a9
Parker, Eric P <eric_parker@harvard.edu>
2015-11-24 12:29:42 -0500

    made some changes based on PR comments. this is also a little experimenting with diff selectors.

--- js/account_settings_disable_add_user_and_course.js
+++ js/account_settings_disable_add_user_and_course.js
@@ -1,34 +1,35 @@
 function isAddUserButtonPresent(){
-    $btn = $('a.add_user_link');
+    $btn = $('#right-side > div.rs-margin-all > a.add_user_link.btn.button-sidebar-wide');
     return $btn.length > 0;
 }
 
 function isAddCourseButtonPresent(){
-    $btn = $('a.add_course_link');
+    $btn = $('#right-side > div.rs-margin-all > a.add_course_link.btn.button-sidebar-wide');
     return $btn.length > 0;
 }
 
 function disableAddUserButton(){
   // remove the href asd well so people can't get the url
   // and try it directly
-  $('a.add_user_link').removeAttr("href").addClass('disabled');
+  $('#right-side > div.rs-margin-all > a.add_user_link.btn.button-sidebar-wide').removeAttr("href").addClass('disabled');
 }
 
 function disableAddCourseButton(){
   // remove the href asd well so people can't get the url
   // and try it directly
-  $('a.add_course_link').removeAttr('href').addClass('disabled');
+  $('#right-side > div.rs-margin-all > a.add_course_link.btn.button-sidebar-wide').removeAttr('href').addClass('disabled');
 }
 
 function initDisableAddUserCourse() {
   // These button appear on multiple pages in the account area.
   // So far I have found them on the root account page, the settings page,
   // and the users page. There could be other pages as well.
-  var reAccountPage = /accounts\/.+?/;
-  var windowUrl = window.location.pathname;
-  var onAccountPage = (windowUrl.search(reAccountPage) != -1);
 
-  if (onAccountPage) {
+  //var reAccountPage = /accounts\/.+?/;
+  //var windowUrl = window.location.pathname;
+  //var onAccountPage = (windowUrl.search(reAccountPage) != -1);
+
+  if ($("body[class*=context-account_]")) {
     if (isAddUserButtonPresent()) {
       disableAddUserButton();
     }
