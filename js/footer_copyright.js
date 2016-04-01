8f00cd22f73347db1b3e0c1f348df96a069dcf34
Dave Bonner <david_bonner@harvard.edu>
2016-04-01 13:23:57 -0400

    update footer customization for new canvas ui
    * removes logo changes, those are being done via the theme editor
      now.

---
+++ js/footer_copyright.js
@@ -0,0 +1,7 @@
+$(document).ready(function(e) {
+    var harvardCopy = 
+        'Copyright &copy; 2016 The President and Fellows of Harvard College | '
+        + '<a href="https://wiki.harvard.edu/confluence/display/canvas/Harvard+Privacy+Policy+for+Canvas" id="privacy_policy_link">Privacy Policy</a> | '
+        + '<a href="https://wiki.harvard.edu/confluence/display/canvas/Harvard+Acceptable+Use+Policy+for+Canvas" id="acceptable_use_policy_link">Acceptable Use Policy</a>';
+    $('footer').html(harvardCopy);
+});
