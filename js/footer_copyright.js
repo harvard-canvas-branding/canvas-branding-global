f3bc9ed4ec522b77573e72aa63a974ea97cc215a
Dave Bonner <david_bonner@harvard.edu>
2016-04-06 09:52:54 -0400

    add comment

--- js/footer_copyright.js
+++ js/footer_copyright.js
@@ -1,4 +1,10 @@
 $(document).ready(function(e) {
+    /* NOTE: with the current (as of 2016-04-06) styling on the canvas UI,
+     * the below copy needs to be wrapped in a div to avoid having the spacing
+     * in between it getting squished down to nothing when the viewport is wide
+     * enough.  also, wrapping things in <p> tags doesn't get you multiple
+     * lines
+     */
     var harvardCopy = 
         '<div>Copyright &copy; 2016 The President and Fellows of Harvard College | ' +
         '<a href="https://wiki.harvard.edu/confluence/display/canvas/Harvard+Privacy+Policy+for+Canvas" id="privacy_policy_link">Privacy Policy</a> | ' +
