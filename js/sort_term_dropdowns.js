039ad92a47b1bb93dca6991a950d40a055e1be40
Colin Murtaugh <colin_murtaugh@harvard.edu>
2016-06-23 20:14:37 -0400

    sort the term dropdown on the main account admin page

---
+++ js/sort_term_dropdowns.js
@@ -0,0 +1,31 @@
+
+function isTermDropdownPresent(){
+  $termDropdown =$('select[name="enrollment_term_id"]');
+  return $termDropdown.length > 0;
+}
+
+function sortTermDropdown(){
+  var selectList = $('select[name="enrollment_term_id"] option');
+
+  var re_letter = /^[A-Za-z]/;
+  var re_number = /^[0-9]/;
+  selectList.sort(function(a,b){
+      // 'All Terms' at the top; other labels beggining with a letter at the end
+      if (a.label == 'All Terms') {
+        return -1;
+      }
+      if ( a.label.match(re_letter) && b.label.match(re_number) ) {
+        return 1;
+      }
+      return (a.label >= b.label) ? -1 : 1;
+  });
+  $('select[name="enrollment_term_id"]').html(selectList);
+}
+
+function initSortTermDropdown() {
+  if (isTermDropdownPresent()) {
+    sortTermDropdown();
+  }
+}
+
+$(document).ready(initSortTermDropdown);
