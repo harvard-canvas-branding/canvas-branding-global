5047693fabdcbae63caca3d173b7f14a474d1777
Colin Murtaugh <colin_murtaugh@harvard.edu>
2016-07-06 14:10:42 -0400

    Simplified code; added comments; moved Ongoing term to position #2 in the list; fixed detection of the presence of the enrollment_term_id select list.

--- js/sort_term_dropdowns.js
+++ js/sort_term_dropdowns.js
@@ -1,14 +1,4 @@
 
-function isTermDropdownPresent(){
-  termDropdown = $('select[name="enrollment_term_id"]');
-  return termDropdown.length > 0;
-}
-
-function isTermTablePresent(){
-  termTable = $('table#terms');
-  return termTable.length > 0;
-}
-
 function sortTermDropdown(){
   var termSelect = $('select[name="enrollment_term_id"]');
   var selectedOption = $(termSelect).val();
@@ -16,19 +6,26 @@ function sortTermDropdown(){
   var re_letter = /^[A-Za-z]/;
   var re_number = /^[0-9]/;
 
-  var topList = new Array;
-  var middleList = new Array;
-  var bottomList = new Array;
+  var topList = new Array();
+  var middleList = new Array();
+  var bottomList = new Array();
 
   for (var i=0; i < termSelectList.length; i++) {
     if (termSelectList[i].label == 'All Terms') {
+      // this should be the first item in the list
+      topList.unshift(termSelectList[i]);
+    }
+    else if (termSelectList[i].label == 'Ongoing') {
+      // this should be the second item in the list
       topList.push(termSelectList[i]);
     }
     else if (termSelectList[i].label.match(re_letter)) {
+      // all other items that start with a letter will go at the bottom, sorted ascending
       bottomList.push(termSelectList[i]);
     }
     else {
-      middleList.push(termSelectList[i])
+      // all of the items that start with a number will go below the first two items, sorted descending
+      middleList.push(termSelectList[i]);
     }
   }
 
@@ -41,8 +38,6 @@ function sortTermDropdown(){
     })
   );
   $(termSelect).html(newList).val(selectedOption);
-
-  //$('select[name="enrollment_term_id"] option:eq(0)').prop('selected',true);
 }
 
 function sortTermTable(){
@@ -54,9 +49,11 @@ function sortTermTable(){
 
   for (var i=0; i < termRowList.length; i++) {
     if (termRowList[i].getElementsByClassName('name')[0].innerHTML.match(re_number)) {
+      // all of the term labels that start with a number are at the top, sorted descending
       topList.push(termRowList[i]);
     }
     else {
+      // term labels that start with a letter are at the bottom, sorted ascending
       bottomList.push(termRowList[i]);
     }
   }
@@ -79,10 +76,10 @@ function sortTermTable(){
 }
 
 $(document).ready(function() {
-  if (isTermDropdownPresent()) {
+  if ($('select[name="enrollment_term_id"]').length > 0) {
     sortTermDropdown();
   }
-  if (isTermTablePresent()) {
+  if ($('table#terms').length > 0) {
     sortTermTable();
   }
 });
