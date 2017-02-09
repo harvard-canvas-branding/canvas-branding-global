0a54dc4d7076f10f1543b7a31a494c5faa9c18f6
Colin Murtaugh <cmurtaugh@gmail.com>
2017-02-09 09:58:24 -0500

    remove the Start a New Course button that may appear on the dashboard

---
+++ js/dashboard_remove_start_course.js
@@ -0,0 +1,21 @@
+function isStartCourseButtonPresent(){
+    $btn = $('#right-side > div > button#start_new_course');
+    return $btn.length > 0;
+}
+
+function removeStartCourseButton(){
+  $('#right-side > div > button#start_new_course').remove();
+}
+
+function initDisableDashboardStartCourse() {
+  // There's a "Start a New Course" button on the dashboard
+  // that we want to remove
+
+  if ($("body[class*=context-user_]")) {
+    if(isStartCourseButtonPresent()){
+      disableStartCourseButton();
+    }
+  }
+}
+
+$(document).ready(initDisableDashboardStartCourse);
