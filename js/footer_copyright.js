8db0e0054e64fb55dcbf2ccb68d089d47735a089
Colin Murtaugh <cmurtaugh@gmail.com>
2017-02-09 09:23:26 -0500

    update the copyright year

--- js/footer_copyright.js
+++ js/footer_copyright.js
@@ -5,8 +5,8 @@ $(document).ready(function(e) {
      * enough.  also, wrapping things in <p> tags doesn't get you multiple
      * lines
      */
-    var harvardCopy = 
-        '<div>Copyright &copy; 2016 The President and Fellows of Harvard College | ' +
+    var harvardCopy =
+        '<div>Copyright &copy; 2017 The President and Fellows of Harvard College | ' +
         '<a href="https://wiki.harvard.edu/confluence/display/canvas/Harvard+Privacy+Policy+for+Canvas" id="privacy_policy_link">Privacy Policy</a> | ' +
         '<a href="https://wiki.harvard.edu/confluence/display/canvas/Harvard+Acceptable+Use+Policy+for+Canvas" id="acceptable_use_policy_link">Acceptable Use Policy</a>' +
         '</div>';
