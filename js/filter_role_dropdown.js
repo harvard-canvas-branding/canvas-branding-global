edc698ef642779967ae7c9d3f7d0f50657685896
Colin Murtaugh <cmurtaugh@gmail.com>
2018-05-04 08:35:10 -0400

    also remove the "None" TRs from the right-hand side of the course settings page

--- js/filter_role_dropdown.js
+++ js/filter_role_dropdown.js
@@ -1,4 +1,18 @@
 $(document).ready(function(e) {
-  // Remove options with 0 enrollments from the role dropdown on the People page
-  $("select[name='enrollment_role_id'] option:contains('(0)')").remove();
+
+  var reCourseSettingsPage = /courses\/.+?\/settings/;
+  var reCoursePeoplePage = /courses\/.+?\/users/;
+  var windowUrl = window.location.pathname;
+  var onCourseSettingsPage = (windowUrl.search(reCourseSettingsPage) != -1);
+  var onCoursePeoplePage = (windowUrl.search(reCoursePeoplePage) != -1);
+
+  if (onCourseSettingsPage) {
+    // Remove TRs in the right-hand column Current Users table that have "None"
+    $("aside#right-side > div > table.summary > tbody > tr > td:contains('None')").parent().hide();
+  }
+
+  if (onCoursePeoplePage) {
+    // Remove options with 0 enrollments from the role dropdown on the People page
+    $("select[name='enrollment_role_id'] option:contains('(0)')").remove();
+  }
 });
