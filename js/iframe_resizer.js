257c1769a94ccce522ce8acdc7b9ac3c5baf8337
Colin Murtaugh <cmurtaugh@gmail.com>
2018-08-17 18:31:12 -0400

    try catching postMessage calls from inside the iframe

--- js/iframe_resizer.js
+++ js/iframe_resizer.js
@@ -12,6 +12,26 @@
   ((function init() {
     if (onEditablePage()) {
       console.log('on an editable page - look for iframes to resize');
+
+      window.addEventListener("message", receiveMessage, false);
+      function receiveMessage(event)
+      {
+        if (event.origin !== "https://localhost:8000") {
+          console.log('bad origin - ignoring: '+event.origin);
+          return;
+        }
+
+        console.log('got a message from '+event.origin+' with a message of '+event.data);
+        // resize the iframes
+        var widgets = $('div#content').find('iframe[title="Course Info"]');
+        if (widgets.length > 0) {
+          widgets.iFrameResize();
+          console.log('resized the iframe');
+        }
+        // ...
+      }
+
+      /*
       var courseInfoObserver = new MutationObserver(function (mutations) {
           console.log(mutations);
           var $widgets = $('div#content').find('iframe[title="Course Info"]');
@@ -29,6 +49,7 @@
       };
 
       courseInfoObserver.observe(document.getElementById('content'), moConfig);
+      */
     }
   })());
 })());
