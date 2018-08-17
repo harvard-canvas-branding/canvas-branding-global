f62a4b4d745ce8914078dc9fd6dae28b509cd9ee
Colin Murtaugh <cmurtaugh@gmail.com>
2018-08-17 14:19:26 -0400

    try that again...

--- js/iframe_resizer.js
+++ js/iframe_resizer.js
@@ -4,22 +4,19 @@
     return ($('body').hasClass('syllabus') || $('body').hasClass('pages'));
   }
 
-  var iframeResizerCDN = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.6.1/iframeResizer.min.js';
-  require(['jquery', iframeResizerCDN], function() {
+  var iframeResizerCDN = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.1/iframeResizer.min.js';
+  require(['jquery', iframeResizerCDN]);
 
   // If we're on a page with the course info iframe, set up bidirectional
   // messaging to keep iframe size in sync with embedded document size
   ((function init() {
-
     if (onEditablePage()) {
       var courseInfoObserver = new MutationObserver(function (mutations) {
           var $widgets = $('div#content').find('iframe[title="Course Info"]');
           if ($widgets.length > 0) {
-              $widgets.iFrameResize();
-              console.log('resized an iframe');
-              // assumes all iframes were added simultaneously
-              courseInfoObserver.disconnect();
-            };
+            $widgets.iFrameResize();
+            // assumes all iframes were added simultaneously
+            courseInfoObserver.disconnect();
           }
       });
 
