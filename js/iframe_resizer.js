19c7ca5487266a51ff8893d38d9dbd12e3a7e16f
Colin Murtaugh <cmurtaugh@gmail.com>
2018-08-17 18:42:01 -0400

    only load the external resizer js where we'll need it

--- js/iframe_resizer.js
+++ js/iframe_resizer.js
@@ -4,8 +4,6 @@
     return ($('body').hasClass('syllabus') || $('body').hasClass('pages'));
   }
 
-  var iframeResizerCDN = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.1/iframeResizer.min.js';
-  require([iframeResizerCDN]);
 
   // If we're on a page with the course info iframe, set up bidirectional
   // messaging to keep iframe size in sync with embedded document size
@@ -13,43 +11,23 @@
     if (onEditablePage()) {
       console.log('on an editable page - look for iframes to resize');
 
+      var iframeResizerCDN = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.1/iframeResizer.min.js';
+      require([iframeResizerCDN]);
+
       window.addEventListener("message", receiveMessage, false);
       function receiveMessage(event)
       {
-        if (event.origin !== "https://localhost:8000") {
-          console.log('bad origin - ignoring: '+event.origin);
+        if (event.origin !== "https://localhost:8000" || event.message !== "resize") {
           return;
         }
 
-        console.log('got a message from '+event.origin+' with a message of '+event.data);
-        // resize the iframes
+        // resize the iframe(s)
         var widgets = $('div#content').find('iframe[title="Course Info"]');
         if (widgets.length > 0) {
           widgets.iFrameResize();
           console.log('resized the iframe');
         }
-        // ...
       }
-
-      /*
-      var courseInfoObserver = new MutationObserver(function (mutations) {
-          console.log(mutations);
-          var $widgets = $('div#content').find('iframe[title="Course Info"]');
-          if ($widgets.length > 0) {
-            $widgets.iFrameResize();
-            console.log('resized an iframe');
-            // assumes all iframes were added simultaneously
-            // courseInfoObserver.disconnect();
-          }
-      });
-
-      var moConfig = {
-        childList: true,
-        subtree: true
-      };
-
-      courseInfoObserver.observe(document.getElementById('content'), moConfig);
-      */
     }
   })());
 })());
