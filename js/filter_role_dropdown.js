5b421688db4a35c4534896a08b8b25e1848efbfd
Colin Murtaugh <cmurtaugh@gmail.com>
2019-06-27 06:52:04 -0400

    trying another mutation observer

--- js/filter_role_dropdown.js
+++ js/filter_role_dropdown.js
@@ -1,7 +1,8 @@
 
-function checkRoleList(mutations, observer) {
+ function addSelectListHandler(mutations, observer) {
   const roleLists = document.getElementsByName('enrollment_role_id');
   if (!roleLists && typeof observer === 'undefined') {
+    console.log('list is not present; adding a mutation observer to watch for it');
     const obs = new MutationObserver(checkRoleList);
     obs.observe(document.body, {
       'childList' : true
@@ -12,9 +13,23 @@ function checkRoleList(mutations, observer) {
       console.log('disconnecting the observer');
       observer.disconnect();
     }
-    console.log('removing the options');
-    $("select[name='enrollment_role_id'] option:contains('(0)')").remove();
+    console.log(`roleLists: ${roleLists}`);
+    for (var dropdown of roleLists) {
+      console.log(`adding the handler to ${dropdown}`);
+      dropdown.onfocus = function() {
+        console.log('in handler');
+        $("select[name='enrollment_role_id'] option:contains('(0)')").remove();
+
+        // for(var i = 0; i < this.options.length; i++) {
+        //   var opt = this.options[i];
+        //   console.log(`removing ${opt.label}`);
+        //   console.log(`${typeof(opt)}`);
+        //   this.remove(i);
+        // }
+      }
+    }
   }
+  console.log('done');
 }
 
 $(document).ready(function(e) {
@@ -33,6 +48,25 @@ $(document).ready(function(e) {
   if (onCoursePeoplePage) {
     // Remove options with 0 enrollments from the role dropdown on the People page
     // $("select[name='enrollment_role_id'] option:contains('(0)')").remove();
-    checkRoleList();
+
+    // add a handler to the dropdown element
+    addSelectListHandler();
+
+    var targetNode = document.getElementsByName('enrollment_role_id')[0];
+    var config = { childList: true, subtree: true };
+    var callback = function(mutationsList, observer) {
+      for (var mutation of mutationsList) {
+        if (mutation.type == 'childList') {
+          console.log('a child node has been added or removed');
+          newNodes = mutation.addedNodes;
+          for (var n of newNodes) {
+            console.log(n.label)
+          }
+        }
+      }
+    }
+    var observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+
   }
 });
