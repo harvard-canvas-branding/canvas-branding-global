60b989c3607af20ad4debf91f6f329070d9a58e6
Colin Murtaugh <cmurtaugh@gmail.com>
2019-06-27 07:22:16 -0400

    try NodeList.item()

--- js/filter_role_dropdown.js
+++ js/filter_role_dropdown.js
@@ -1,33 +1,33 @@
 
- function addSelectListHandler(mutations, observer) {
-  const roleLists = document.getElementsByName('enrollment_role_id');
-  if (!roleLists && typeof observer === 'undefined') {
+function addSelectListHandler(mutations, observer) {
+  console.log('addSelectListHandler called');
+  const roleList = document.getElementsByName('enrollment_role_id').item(0);
+  if (!roleList && typeof observer === 'undefined') {
     console.log('list is not present; adding a mutation observer to watch for it');
     const obs = new MutationObserver(checkRoleList);
     obs.observe(document.body, {
       'childList' : true
     });
   }
-  if (roleLists) {
+  if (roleList) {
     if (typeof observer !== 'undefined') {
       console.log('disconnecting the observer');
       observer.disconnect();
     }
-    console.log(`roleLists: ${roleLists}`);
-    for (var dropdown of roleLists) {
-      console.log(`adding the handler to ${dropdown}`);
-      dropdown.onfocus = function() {
-        console.log('in handler');
-        $("select[name='enrollment_role_id'] option:contains('(0)')").remove();
+    console.log(`roleLists: ${roleList}`);
+    console.log(`adding the handler to ${roleList}`);
+    roleList.onfocus = function() {
+      console.log('in handler');
+      $("select[name='enrollment_role_id'] option:contains('(0)')").remove();
 
-        // for(var i = 0; i < this.options.length; i++) {
-        //   var opt = this.options[i];
-        //   console.log(`removing ${opt.label}`);
-        //   console.log(`${typeof(opt)}`);
-        //   this.remove(i);
-        // }
-      }
+      // for(var i = 0; i < this.options.length; i++) {
+      //   var opt = this.options[i];
+      //   console.log(`removing ${opt.label}`);
+      //   console.log(`${typeof(opt)}`);
+      //   this.remove(i);
+      // }
     }
+
   }
   console.log('done');
 }
@@ -52,21 +52,21 @@ $(document).ready(function(e) {
     // add a handler to the dropdown element
     addSelectListHandler();
 
-    var targetNode = document.getElementsByName('enrollment_role_id')[0];
-    var config = { childList: true, subtree: true };
-    var callback = function(mutationsList, observer) {
-      for (var mutation of mutationsList) {
-        if (mutation.type == 'childList') {
-          console.log('a child node has been added or removed');
-          newNodes = mutation.addedNodes;
-          for (var n of newNodes) {
-            console.log(n.label)
-          }
-        }
-      }
-    }
-    var observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
+    // var targetNode = document.getElementsByName('enrollment_role_id')[0];
+    // var config = { childList: true, subtree: true };
+    // var callback = function(mutationsList, observer) {
+    //   for (var mutation of mutationsList) {
+    //     if (mutation.type == 'childList') {
+    //       console.log('a child node has been added or removed');
+    //       newNodes = mutation.addedNodes;
+    //       for (var n of newNodes) {
+    //         console.log(n.label)
+    //       }
+    //     }
+    //   }
+    // }
+    // var observer = new MutationObserver(callback);
+    // observer.observe(targetNode, config);
 
   }
 });
