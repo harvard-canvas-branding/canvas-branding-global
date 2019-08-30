a01d9729740d1dc22fbb8421d732166376f87a88
Colin Murtaugh <cmurtaugh@gmail.com>
2019-08-30 15:21:48 -0400

    first pass at changing nav formatting

--- js/left_nav_tool_visibility_indicator.js
+++ js/left_nav_tool_visibility_indicator.js
@@ -11,20 +11,22 @@ var huToolVisibilityRestricted = [
 
 function huFuzzyVisUpdate(tool){
   return $("div[id='left-side'] a[class^='context_external_tool']:contains('"+ tool + "')")
-    .parent().addClass('section-tab-hidden');
+    .append('<i class="nav-icon icon-off" aria-hidden="true" role="presentation"></i>')
+    // .parent().addClass('section-tab-hidden');
 }
 
 function huExactVisUpdate(tool){
   return $("div[id='left-side'] a[class^='context_external_tool']")
     .filter(function(){ return $(this).text() === tool })
-    .parent().addClass('section-tab-hidden');
+    .append('<i class="nav-icon icon-off" aria-hidden="true" role="presentation"></i>')
+    // .parent().addClass('section-tab-hidden');
 }
 
 
 /*
  Changes the link style of a specified list of custom tools in the left-hand nav
   to indicate they are not visible to students (current Canvas behavior is to show
-  the link text in grey).
+  an icon to the right of the text).
   toolList: Array of strings representing the link text of all
             tools that need to be marked as visible only to admins.
   exactMatch: If true, will only match tool links with the exact names in toolList.
