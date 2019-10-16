1d15d71b31b05f18bf7bd06db326d6f3986c3f4d
Sapna Mysore <sapna_mysore@harvard.edu>
2019-10-16 12:15:36 -0400

    Modified the tool element title

--- js/left_nav_tool_visibility_indicator.js
+++ js/left_nav_tool_visibility_indicator.js
@@ -8,19 +8,21 @@ var huToolVisibilityRestricted = [
   "Import iSites Content",
   "Manage Course",
 ];
-
 function huFuzzyVisUpdate(tool){
-  return $("div[id='left-side'] a[class^='context_external_tool']:contains('"+ tool + "')")
-    .append('<i class="nav-icon icon-off" title="Not visible to students"  aria-hidden="true" role="presentation"></i>')
+	var element = $("div[id='left-side'] a[class^='context_external_tool']:contains('"+ tool + "')");
+	//modify the title and append the icon
+	element.attr("title", "Not visible to students");
+	return element.append('<i class="nav-icon icon-off" title="Not visible to students"  aria-hidden="true" role="presentation"></i>')
 }
 
 function huExactVisUpdate(tool){
-  return $("div[id='left-side'] a[class^='context_external_tool']")
-    .filter(function(){ return $(this).text() === tool })
-    .append('<i class="nav-icon icon-off"  title="Not visible to students" aria-hidden="true" role="presentation"></i>')
+	var element = $("div[id='left-side'] a[class^='context_external_tool']")
+    .filter(function(){ return $(this).text() === tool });
+	//modify the title and append the icon
+	element.attr("title", "Not visible to students");
+  	return element.append('<i class="nav-icon icon-off"  title="Not visible to students" aria-hidden="true" role="presentation"></i>')
 }
 
-  
 /*
  Changes the link style of a specified list of custom tools in the left-hand nav
   to indicate they are not visible to students (current Canvas behavior is to show
