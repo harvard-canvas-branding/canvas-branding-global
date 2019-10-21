c9803c2c924db04178ba179827a18ca868f1341d
Colin Murtaugh <cmurtaugh@gmail.com>
2019-10-21 11:12:22 -0400

    Use the in-page Google Analytics object rather than loading it again; set anonymizeIp to true

--- js/google_analytics.js
+++ js/google_analytics.js
@@ -1,30 +1,28 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','harvardga');
+if (window.ga && ga.loaded) {
+  try {
+    ga('create', 'UA-49649810-4', 'auto', 'huTracker');
+    ga('huTracker.set', 'anonymizeIp', true);
 
-harvardga('create', 'UA-49649810-4', 'auto');
-try {
-  if (typeof ENV !== 'undefined') {
-    harvardga('set', 'dimension1', ENV.COURSE_ID);
+    if (typeof ENV !== 'undefined') {
+      ga('huTracker.set', 'dimension1', ENV.COURSE_ID);
 
-    if ( ENV.current_user_roles.indexOf('admin') != -1 ) {
-      harvardga('set', 'dimension2', 'admin');
+      if ( ENV.current_user_roles.indexOf('admin') != -1 ) {
+        ga('huTracker.set', 'dimension2', 'admin');
+      }
+      else if ( ENV.current_user_roles.indexOf('teacher') != -1 ) {
+        ga('huTracker.set', 'dimension2', 'teacher');
+      }
+      else if ( ENV.current_user_roles.indexOf('student') != -1 ) {
+        ga('huTracker.set', 'dimension2', 'student');
+      }
+      else {
+        ga('huTracker.set', 'dimension2', 'user');
+      }
     }
-    else if ( ENV.current_user_roles.indexOf('teacher') != -1 ) {
-      harvardga('set', 'dimension2', 'teacher');
-    }
-    else if ( ENV.current_user_roles.indexOf('student') != -1 ) {
-      harvardga('set', 'dimension2', 'student');
-    }
-    else {
-      harvardga('set', 'dimension2', 'user');
-    }
+
+    ga('huTracker.send', 'pageview');
+  }
+  catch(err) {
+    // do nothing
   }
 }
-catch(err) {
-  // do nothing
-}
-
-
-harvardga('send', 'pageview');
