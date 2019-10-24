ca893ce85317d7934bbc6f0517e11744a8f6c7a6
Colin Murtaugh <cmurtaugh@gmail.com>
2019-10-24 11:26:55 -0400

    try getting rid of the document.ready wrapper

--- js/google_analytics.js
+++ js/google_analytics.js
@@ -1,32 +1,31 @@
 
-$(document).ready(function(e) {
+if (window.ga) {
+  try {
+    ga('create', 'UA-49649810-4', 'auto', 'huTracker');
+    ga('huTracker.set', 'anonymizeIp', true);
 
-  if (window.ga) {
-    try {
-      ga('create', 'UA-49649810-4', 'auto', 'huTracker');
-      ga('huTracker.set', 'anonymizeIp', true);
+    if (typeof ENV !== 'undefined') {
+      ga('huTracker.set', 'dimension1', ENV.COURSE_ID);
 
-      if (typeof ENV !== 'undefined') {
-        ga('huTracker.set', 'dimension1', ENV.COURSE_ID);
-
-        if ( ENV.current_user_roles.indexOf('admin') != -1 ) {
-          ga('huTracker.set', 'dimension2', 'admin');
-        }
-        else if ( ENV.current_user_roles.indexOf('teacher') != -1 ) {
-          ga('huTracker.set', 'dimension2', 'teacher');
-        }
-        else if ( ENV.current_user_roles.indexOf('student') != -1 ) {
-          ga('huTracker.set', 'dimension2', 'student');
-        }
-        else {
-          ga('huTracker.set', 'dimension2', 'user');
-        }
+      if ( ENV.current_user_roles.indexOf('admin') != -1 ) {
+        ga('huTracker.set', 'dimension2', 'admin');
       }
-      ga('huTracker.send', 'pageview');
+      else if ( ENV.current_user_roles.indexOf('teacher') != -1 ) {
+        ga('huTracker.set', 'dimension2', 'teacher');
+      }
+      else if ( ENV.current_user_roles.indexOf('student') != -1 ) {
+        ga('huTracker.set', 'dimension2', 'student');
+      }
+      else {
+        ga('huTracker.set', 'dimension2', 'user');
+      }
     }
-    catch(err) {
-      // do nothing
-      console.log('huTracker failed');
-    }
+    ga('huTracker.send', 'pageview');
   }
-});
+  catch(err) {
+    // do nothing
+    console.log('huTracker failed');
+  }
+} else {
+  console.log('window.ga was not present');
+}
