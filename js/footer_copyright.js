3a7d2555c045615b134786bc8e6e312106d9d95d
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2019-12-18 10:20:13 -0500

    Add accessibility link to Canvas footer

--- js/footer_copyright.js
+++ js/footer_copyright.js
@@ -9,8 +9,9 @@ $(document).ready(function(e) {
 
     var harvardCopy =
         '<div>Copyright &copy; 2013-' + copyYear + ' The President and Fellows of Harvard College | ' +
-        '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=3373044ddb4d57cc83a2f3f7bf961909" id="privacy_policy_link" target="_blank">Privacy Policy</a> | ' +
         '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=9d718485db0d57cc83a2f3f7bf961902" id="acceptable_use_policy_link" target="_blank">Acceptable Use Policy</a>' +
+        '<a href="https://accessibility.huit.harvard.edu/digital-accessibility-policy" id="acceptable_use_policy_link" target="_blank">Accessibility</a>'+
+        '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=3373044ddb4d57cc83a2f3f7bf961909" id="privacy_policy_link" target="_blank">Privacy Policy</a> | ' +
         '</div>';
     $('footer').html(harvardCopy);
 });
