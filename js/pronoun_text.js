53fd46df4ce6776bd398bdffc4c5e0abfe8dcdbd
Colin Murtaugh <cmurtaugh@gmail.com>
2021-04-22 10:17:09 -0400

    "this application" --> "Canvas"

--- js/pronoun_text.js
+++ js/pronoun_text.js
@@ -10,12 +10,12 @@ function initAddPronounText() {
     <br/>This pronoun will appear after your name when enabled.\
     If you would like to do so, use this section to identify, \
     change, or delete your chosen personal pronouns. Currently \
-    this application requires users to choose from a limited \
+    Canvas requires users to choose from a limited \
     prepopulated list of personal pronouns. We respect that \
     language is constantly evolving and members of our community \
     should be able to be called and describe their identity \
     however they choose, so we are working to enable that option \
-    within this application. To learn more about gender, identity, \
+    within Canvas. To learn more about gender, identity, \
     or pronouns and to provide feedback, visit: \
     <a href=\"https://dib.harvard.edu/gender-pronouns\">\
     https://dib.harvard.edu/gender-pronouns</a>.\
