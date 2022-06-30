c0dcd0b141d83ebc69047395b2f1318bfd7806e6
Patrick Tonne <patrick_tonne@harvard.edu>
2022-06-30 15:20:28 -0400

    update guidance link on Canvas Analytics page

--- js/analytics_text.js
+++ js/analytics_text.js
@@ -16,7 +16,7 @@ function initAnalyticsTextBlock(){
 	var guidance_text = '<div id="analytics-guidance" class="tlt-analytics-center-alert-info">' +
 	'	<div class="tlt-analytics-alert-info">' +
 	'		The Faculty Oversight Committee on the Access to Electronic Information provides' +
-	' 		<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=dc7fa896db8d170083a2f3f7bf96198f" target="_blank">' +
+	' 		<a href="https://huit.harvard.edu/privacy/canvas#oversight" target="_blank">' +
 	' 		<strong>guidance to faculty</strong></a> on using these analytics.' +
 	'	</div>' +
 	'</div>';
