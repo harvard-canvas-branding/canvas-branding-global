6f0e673ff1cb591673032878cdb3a5d5112af5f3
Haydn <haydn_ady@harvard.edu>
2024-01-30 22:15:20 -0500

    Updated to add footer on pages missing it and minor formatting

--- js/footer_copyright.js
+++ js/footer_copyright.js
@@ -1,26 +1,40 @@
-$(document).ready(function(e) {
-    /* NOTE: with the current (as of 2016-04-06) styling on the canvas UI,
-     * the below copy needs to be wrapped in a div to avoid having the spacing
-     * in between it getting squished down to nothing when the viewport is wide
-     * enough.  also, wrapping things in <p> tags doesn't get you multiple
-     * lines
-     */
-    function addFooterContent() {
-        const copyYear = new Date().getFullYear();
-  
-        const harvardCopy =
-          "<div id='custom-footer-container'>" +
-            '<footer role="contentinfo" id="footer" class="ic-app-footer">' +
-              "<div>***TEST Copyright &copy; 2013-" + copyYear + " The President and Fellows of Harvard College | " +
-                '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=9d718485db0d57cc83a2f3f7bf961902" id="acceptable_use_policy_link" target="_blank">Acceptable Use Policy</a> | ' +
-                '<a href="https://accessibility.huit.harvard.edu/digital-accessibility-policy" id="accessibility_link" target="_blank">Accessibility</a> | ' +
-                '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=3373044ddb4d57cc83a2f3f7bf961909" id="privacy_policy_link" target="_blank">Privacy Policy</a>' +
-              "</div>" +
-            '</footer>' +
-          "</div>";
-  
-        $("#content-wrapper").append(harvardCopy);
-      }
+$(document).ready(function (e) {
+  /* NOTE: with the current (as of 2016-04-06) styling on the canvas UI,
+   * the below copy needs to be wrapped in a div to avoid having the spacing
+   * in between it getting squished down to nothing when the viewport is wide
+   * enough.  also, wrapping things in <p> tags doesn't get you multiple
+   * lines
+   */
+  function addFooterContent() {
+    const copyYear = new Date().getFullYear();
 
-      addFooterContent();
+    const harvardCopy =
+      "<div>***TEST Copyright &copy; 2013-" +
+      copyYear +
+      " The President and Fellows of Harvard College | " +
+      '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=9d718485db0d57cc83a2f3f7bf961902" id="acceptable_use_policy_link" target="_blank">Acceptable Use Policy</a> | ' +
+      '<a href="https://accessibility.huit.harvard.edu/digital-accessibility-policy" id="accessibility_link" target="_blank">Accessibility</a> | ' +
+      '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=3373044ddb4d57cc83a2f3f7bf961909" id="privacy_policy_link" target="_blank">Privacy Policy</a>' +
+      "</div>";
+
+    // Check for <footer> element on page.
+    if ($("footer").length > 0) {
+      // Select and replace the HTML content inside all <footer> element with the content of the `harvardCopy`.
+      $("footer").html(harvardCopy);
+    } else {
+      // Create <footer> element.
+      const harvardCopy2 =
+        '<div id="custom-footer-container">' +
+        '<footer role="contentinfo" id="footer" class="ic-app-footer">' +
+        harvardCopy +
+        "</footer>" +
+        "</div>";
+
+      // Append <footer> element to div content-wrapper element and make sure it is visible.
+      $("#content-wrapper").append(harvardCopy2);
+      $("#footer").css("display", "block");
+    }
+  }
+
+  addFooterContent();
 });
