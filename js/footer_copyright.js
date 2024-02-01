3ceda1d7b1d1448a3bfc3e00fbef2b344d86d576
Haydn <haydn_ady@harvard.edu>
2024-02-01 17:04:48 -0500

    More unique footer id and removed test prefix

--- js/footer_copyright.js
+++ js/footer_copyright.js
@@ -9,7 +9,7 @@ $(document).ready(function (e) {
     const copyYear = new Date().getFullYear();
 
     const harvardCopy =
-      "<div>***TEST Copyright &copy; 2013-" + copyYear + " The President and Fellows of Harvard College | " +
+      "<div>Copyright &copy; 2013-" + copyYear + " The President and Fellows of Harvard College | " +
         '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=9d718485db0d57cc83a2f3f7bf961902" id="acceptable_use_policy_link" target="_blank">Acceptable Use Policy</a> | ' +
         '<a href="https://accessibility.huit.harvard.edu/digital-accessibility-policy" id="accessibility_link" target="_blank">Accessibility</a> | ' +
         '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=3373044ddb4d57cc83a2f3f7bf961909" id="privacy_policy_link" target="_blank">Privacy Policy</a>' +
@@ -23,14 +23,14 @@ $(document).ready(function (e) {
       // Create <footer> element.
       const harvardCopy2 =
         '<div id="custom-footer-container">' +
-            '<footer role="contentinfo" id="footer" class="ic-app-footer">' +
+            '<footer role="contentinfo" id="hu-footer" class="ic-app-footer">' +
                 harvardCopy +
             "</footer>" +
         "</div>";
 
       // Append <footer> element to div content-wrapper element and make sure it is visible.
       $("#content-wrapper").append(harvardCopy2);
-      $("#footer").css("display", "block");
+      $("#hu-footer").css("display", "block");
     }
   }
 
