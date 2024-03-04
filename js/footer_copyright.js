4e9f679aea1732ef22eb6770a3f598765e9c7109
Haydn <haydn_ady@harvard.edu>
2024-03-04 16:35:17 -0500

    Added `Accessibility` and `Digital Accessibility` links

--- js/footer_copyright.js
+++ js/footer_copyright.js
@@ -11,7 +11,8 @@ $(document).ready(function (e) {
     const harvardCopy =
       "<div>Copyright &copy; 2013-" + copyYear + " The President and Fellows of Harvard College | " +
         '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=9d718485db0d57cc83a2f3f7bf961902" id="acceptable_use_policy_link" target="_blank">Acceptable Use Policy</a> | ' +
-        '<a href="https://accessibility.huit.harvard.edu/digital-accessibility-policy" id="accessibility_link" target="_blank">Accessibility</a> | ' +
+        '<a href="https://accessibility.harvard.edu/" id="accessibility_link" target="_blank">Accessibility</a> | ' +
+        '<a href="https://accessibility.huit.harvard.edu/digital-accessibility-policy" id="digital_accessibility_link" target="_blank">Digital Accessibility</a> | ' +
         '<a href="https://harvard.service-now.com/ithelp?id=kb_article&sys_id=3373044ddb4d57cc83a2f3f7bf961909" id="privacy_policy_link" target="_blank">Privacy Policy</a>' +
       "</div>";
 
